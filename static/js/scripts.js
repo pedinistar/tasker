b8eaed69536b347cbcfa9db50eaf8003e2cdcf5b
Jayashree <pedini14sneha@gmail.com>
2024-09-01 21:42:34 +0530

    analytics update

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,7 @@
+import { inject } from '@vercel/analytics';
+ 
+inject();
+
 document.addEventListener('DOMContentLoaded', function () {
     let deleteForm = null;
     const deleteModal = new bootstrap.Modal(document.getElementById('deleteConfirmationModal'));
